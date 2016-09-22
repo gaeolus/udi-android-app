201fabd1641ceec5cb971a5a85c2d41cd0f8b01f
Ben Smith <bensmith4188@gmail.com>
2016-09-21 21:12:47 -0500

    WIP: connect details presenter to loading device from API

--- app/src/main/java/com/smithsocial/udisampleapp/presenters/DetailsPresenterImpl.java
+++ app/src/main/java/com/smithsocial/udisampleapp/presenters/DetailsPresenterImpl.java
@@ -2,21 +2,27 @@ package com.smithsocial.udisampleapp.presenters;
 
 import android.widget.Button;
 
+import com.smithsocial.udisampleapp.models.LoadDeviceFromApi;
+import com.smithsocial.udisampleapp.models.LoadDeviceFromApiImpl;
 import com.smithsocial.udisampleapp.views.DeviceDetailsActivity;
 
 public class DetailsPresenterImpl extends DetailsPresenter {
     private DeviceDetailsActivity deviceDetailsActivity;
-    //private LoadDeviceFromApi loadDeviceFromApi;
+    private LoadDeviceFromApi loadDeviceFromApi;
 
     public DetailsPresenterImpl(DeviceDetailsActivity deviceDetailsActivity){
         this.deviceDetailsActivity = deviceDetailsActivity;
+        this.loadDeviceFromApi = new LoadDeviceFromApiImpl();
     }
 
     @Override
     public void onResume() {
         if (deviceDetailsActivity != null){
-            deviceDetailsActivity.showProgress();
         }
+    }
+
+    private void reactToDeviceLoad(){
+        // wait until the wrapper is added
     }
 
     @Override
