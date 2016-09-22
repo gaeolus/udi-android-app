8d71c58c7ed9e740dddf9d29295b499dfaaab143
Ben Smith <bensmith4188@gmail.com>
2016-09-22 12:49:50 -0500

    WIP: build the view
    This XML should be good. the Device Details Activity has
    been updated to set the new TextViews in the XML

--- app/src/main/java/com/smithsocial/udisampleapp/views/DeviceDetailsActivity.java
+++ app/src/main/java/com/smithsocial/udisampleapp/views/DeviceDetailsActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.smithsocial.udisampleapp.R;
 import com.smithsocial.udisampleapp.presenters.DetailsPresenter;
@@ -12,6 +14,11 @@ import com.smithsocial.udisampleapp.presenters.DetailsPresenterImpl;
 
 public class DeviceDetailsActivity extends AppCompatActivity implements DetailsView.UpdateUI {
     private ProgressBar progressBar;
+    private TextView brandNameTextView;
+    private TextView deviceIdTextView;
+    private TextView sterilizationBoolTextView;
+    private TextView expirationBoolTextView;
+    private CheckBox deviceSavedCheckBox;
     private DetailsPresenter detailsPresenter;
 
     @Override
@@ -22,7 +29,15 @@ public class DeviceDetailsActivity extends AppCompatActivity implements DetailsV
         setSupportActionBar(toolbar);
 
         progressBar = (ProgressBar) findViewById(R.id.details_progress_bar);
-        detailsPresenter = new DetailsPresenterImpl(this);
+        brandNameTextView = (TextView) findViewById(R.id.details_brand_name);
+        deviceIdTextView = (TextView) findViewById(R.id.details_device_id);
+        sterilizationBoolTextView = (TextView) findViewById(R.id.details_sterilize_prior);
+        expirationBoolTextView = (TextView) findViewById(R.id.details_expiration_bool);
+        deviceSavedCheckBox = (CheckBox) findViewById(R.id.details_save_check_box);
+
+        String deviceId = getIntent().getExtras().getString("device_id");
+        //placeholder device name
+        detailsPresenter = new DetailsPresenterImpl(this, "device name", deviceId);
 
     }
 
@@ -30,6 +45,7 @@ public class DeviceDetailsActivity extends AppCompatActivity implements DetailsV
     protected void onResume() {
         super.onResume();
         detailsPresenter.onResume();
+        detailsPresenter.reactToSave(deviceSavedCheckBox);
     }
 
     @Override
@@ -49,7 +65,28 @@ public class DeviceDetailsActivity extends AppCompatActivity implements DetailsV
     }
 
     @Override
-    public void showDeviceDetails() {
-        // show the device
+    public void setDeviceName(String name) {
+        brandNameTextView.setText(name);
     }
+
+    @Override
+    public void setDeviceId(String deviceId) {
+        deviceIdTextView.setText(deviceId);
+    }
+
+    @Override
+    public void setDeviceExpirationBool(Boolean hasExpiration) {
+        expirationBoolTextView.setText(hasExpiration.toString());
+    }
+
+    @Override
+    public void setSterilizePriorToUse(Boolean hasSterilize) {
+        sterilizationBoolTextView.setText(hasSterilize.toString());
+    }
+
+    @Override
+    public void setDeviceIsSaved(Boolean deviceIsSaved){
+        deviceSavedCheckBox.setChecked(deviceIsSaved);
+    }
+
 }
