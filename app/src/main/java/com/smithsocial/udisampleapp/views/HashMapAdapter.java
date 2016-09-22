e35b533140ca29831bdef0c24cac87a0f39a5c3a
Ben Smith <bensmith4188@gmail.com>
2016-09-22 14:10:17 -0500

    WIP: restructure list item layout xml

---
+++ app/src/main/java/com/smithsocial/udisampleapp/views/HashMapAdapter.java
@@ -0,0 +1,10 @@
+package com.smithsocial.udisampleapp.views;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by ben on 9/22/16.
+ */
+
+public class HashMapAdapter extends ArrayAdapter {
+}
