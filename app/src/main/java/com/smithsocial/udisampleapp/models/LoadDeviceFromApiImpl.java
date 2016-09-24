599ec42f9a003199f005e5c7c5eb146bbeb52fe2
Ben Smith <bensmith4188@gmail.com>
2016-09-23 21:19:05 -0500

    WIP: return whether the device exists and a real device

--- app/src/main/java/com/smithsocial/udisampleapp/models/LoadDeviceFromApiImpl.java
+++ app/src/main/java/com/smithsocial/udisampleapp/models/LoadDeviceFromApiImpl.java
@@ -1,13 +1,16 @@
 package com.smithsocial.udisampleapp.models;
 
+import udiwrapper.Device.Device;
+import udiwrapper.UDIWrapper;
+
 public class LoadDeviceFromApiImpl extends LoadDeviceFromApi {
     @Override
     public boolean DeviceExists(String deviceId) {
-        return false; //return whether or not the device exists
+        return UDIWrapper.deviceExists(deviceId);
     }
 
     @Override
-    public void getDevice(String deviceId) {
-        // return a device with a given DI string
+    public Device getDevice(String deviceId) {
+        return UDIWrapper.fetchDIDevice(deviceId);
     }
 }
